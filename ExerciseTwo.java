8f615dde12f0edaf8f05ba81f50638826653ef52
Matty <mattytv11@gmail.com>
2015-06-24 17:37:34 +0100

    phrase builder + numbers

---
+++ ExerciseTwo.java
@@ -0,0 +1,30 @@
+class ExerciseTwo {
+	public static void main (String [] args) {
+		int x = 0;
+		int y = 0;
+		while ( x < 5 ) {
+			// FIRST
+			//y = x - y;
+			// SECOND
+			//y = y + x;
+			// THIRD
+			//x = x + 1;
+			//y = y + x;
+			// FOURTH
+			//y = y + 2;
+			//if( y > 4 ) {
+			//	y = y - 1;
+			//}
+			// FIFTH
+			if ( y < 5 ) {
+				x = x + 1;
+				if ( y < 3 ) {
+					x = x - 1;
+				}
+			}
+			y = y + 2;
+			System.out.print(x + "" + y +" ");
+			x = x + 1;
+		}
+	}
+}
